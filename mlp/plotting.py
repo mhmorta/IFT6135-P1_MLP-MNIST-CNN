25bed2880aed7ca37d7dd681daf0dbb19ca1aa77
Denis Ergashbaev <denis.ergashbaev@gmail.com>
2019-02-10 17:14:17 -0500

    report intermediary

--- mlp/plotting.py
+++ mlp/plotting.py
@@ -1,9 +1,9 @@
 from matplotlib import pyplot as plt
 import numpy as np
 
-reports = [('zeros', 'report_epochs=10,hidden_dims=[500,600],mu=0.01,batch_size=32,weight_init=zeros.csv'),
-           ('normal', 'report_epochs=10,hidden_dims=[500,600],mu=0.01,batch_size=32,weight_init=normal.csv'),
-           ('glorot', 'report_epochs=10,hidden_dims=[500,600],mu=0.01,batch_size=32,weight_init=glorot.csv')
+reports = [
+           ('normal', 'report_epochs=10,hidden_dims=[500,600],mu=0.1,batch_size=32,weight_init=normal.csv'),
+           ('glorot', 'report_epochs=10,hidden_dims=[500,600],mu=0.1,batch_size=32,weight_init=glorot.csv')
 ]
 
 for report in reports:
@@ -15,7 +15,7 @@ for report in reports:
         plt.plot(train, label=weight_init)
 plt.xticks(range(len(train)))
 plt.legend()
-plt.title('Average loss (on training data)')
+plt.title('Average loss (on training data, mu=0.01)')
 plt.xlabel('Epoch')
 plt.ylabel('Average loss')
 plt.show()
