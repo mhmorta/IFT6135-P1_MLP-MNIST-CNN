fixed plotting of error instead of average_loss

mlp/plotting_gradients.py
@@ -5,8 +5,8 @@ import numpy as np
 with open("reports/gradients/{}".format('gradient_epochs=10,hidden_dims=[700,500],mu=0.3112553202446868,batch_size=1,validate_gradient=True,weight_init=glorot.csv'), "r") as f:
     stats = np.loadtxt(f, delimiter=",", skiprows=1)
     plt.plot(stats[:, 2], 'o')
-plt.xticks(range(len(stats[:,0])), stats[:, 0], rotation='vertical')
-plt.title('Maximum difference between gradients')
+plt.xticks(range(len(stats[:, 0])), ['%.f' % x for x in stats[:, 0]], rotation='vertical')
+plt.title('Maximum difference between analytic and numerical gradients')
 plt.xlabel('N')
 plt.ylabel('Difference')
 plt.show()

mlp/plotting_hyperparam_search.py
@@ -4,7 +4,7 @@ import os
 
 
 reports = [f.replace('.csv', '') for f in os.listdir("reports/hyperparams")]
-
+reports = sorted(reports)
 best_model = None
 y_pos = np.arange(len(reports))
 template = "{0:50}|{1:10}"
@@ -19,7 +19,7 @@ for idx, hp in enumerate(reports):
             best_model = (hp, accuracy)
         plt.bar(idx, accuracy, label=hp)
 
-print('\nBest model with params ', best_model[0], ' has accuracy of ', best_model[1])
+print('\nBest model with params\n', best_model[0], '\nhas accuracy of', best_model[1])
 plt.xticks(y_pos, [r.replace(',m','\nm') for r in reports], rotation=90)
 plt.title('Accuracy of different models on validation set')
 plt.xlabel('Hypeparameters')

mlp/plotting_weight_inits_zoomed.py
@@ -1,21 +1,21 @@
 from matplotlib import pyplot as plt
 import numpy as np
 
-reports = [('zeros', 'report_epochs=10,hidden_dims=[500,600],mu=0.01,batch_size=32,weight_init=zeros.csv'),
-           ('normal', 'report_epochs=10,hidden_dims=[500,600],mu=0.01,batch_size=32,weight_init=normal.csv'),
+reports = [('normal', 'report_epochs=10,hidden_dims=[500,600],mu=0.01,batch_size=32,weight_init=normal.csv'),
            ('glorot', 'report_epochs=10,hidden_dims=[500,600],mu=0.01,batch_size=32,weight_init=glorot.csv')
 ]
 
 for report in reports:
     weight_init, file_name = report
+    start_epoch_idx = 3
     with open("reports/weight_inits/{}".format(file_name), "r") as f:
         stats = np.loadtxt(f, delimiter=",", skiprows=1)
-        train = stats[:, 2]
+        train = stats[start_epoch_idx:, 2]
         print(weight_init, '\n', train)
         plt.plot(train, label=weight_init)
-plt.xticks(range(len(stats)), range(1, len(stats)+1))
+plt.xticks(range(len(train)), range(start_epoch_idx + 1, len(stats) + 1))
 plt.legend()
-plt.title('Average loss (on training data, mu=0.01)')
+plt.title('Average loss zoomed from epoch {} (on training data, mu=0.01)'.format(start_epoch_idx+1))
 plt.xlabel('Epoch')
 plt.ylabel('Average loss')
 plt.grid()